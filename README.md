996c74d4e28e99bc23ad43de8ed8c8da48496e6a
Philip Ingrey <phil@apical.co.uk>
2015-10-14 09:31:36 +0100

    add README

---
+++ README.md
@@ -0,0 +1,25 @@
+# Technical Test - Philip Ingrey (Oct 2015)
+
+This module exposes one function that when called on a positive number returns:
+
+    isPrime - [Boolean] whether the integer is prime
+    pFactors - [Array] a list of prime factors
+    pFactorize - [Object] Where the keys are the prime factors and the values are their respective factor number 
+
+## Notes
+
+### Algorithm
+
+This code is based on the fact that if a number is not divisible by all the primes less than it's sqrt then the number is prime. By storing the discovered primes between runs large speed-ups can occur.
+
+### Memory usage
+
+The system saves the primes it has found to speed up future requests. A [rule of thumb](http://mathworld.wolfram.com/PrimeCountingFunction.html) for memory usage will be (if N is the largest tested number):
+
+     16*(N^.5)/ln(N) Bytes
+
+For large N, e.g. ~1.5GB for N=2^64
+ 
+### Valid range
+
+Over 2^53-1 javascripts number format breaks down for integers and a arbitrary precision module like [BigInteger](https://github.com/peterolson/BigInteger.js/) would need to be employed.
--- p.js
+++ p.js
@@ -1,32 +1,3 @@
-//
-// Technical Test - Philip Ingrey
-// Oct 2015
-//
-// This module exposes one function that when called on a positive number returns:
-//     isPrime - [Boolean] whether the integer is prime
-//     pFactors - [Array] a list of prime factors
-//     pFactorize - [Object] Where the keys are the prime factors and the values are their respective factor number 
-//
-
-// NOTES:
-//
-// Algorithm:
-// This code is based on the fact that if a number is not divisible by all the primes
-// less than it's sqrt then the number is prime. By storing the discovered primes between
-// runs large speed-ups can occur.
-//
-//
-// Memory usage:
-// The system saves the primes it has found to speed up future requests.
-// A rule of thumb for memory usage will be (if N is the largest tested number):
-//     16*(N^.5)/ln(N) Bytes
-// For large N, e.g. ~1.5GB for N=2^64
-// 
-// Over 2^53-1 javascripts number format breaks down for integers and a arbitrary precision module like
-//    https://github.com/peterolson/BigInteger.js/blob/master/LICENSE
-// would need to be employed
-//
-
 p = (function(){
     // Starting list of cached primes
     primeList = [2,3,5,7,9,11];
